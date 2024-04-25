9d94e99d79eed6c39f9d8ac048f69329df5dacae
Marzukh Akib Asjad <41894349+MarzukhAsjad@users.noreply.github.com>
2024-04-25 11:03:10 +0800

    Add config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Configuration for main.py
+BLEND_PATH = r'C:\Users\User\Desktop\FYP\flask-app\Xbot.blend'
+IMPORT_PATH = r'C:\Users\User\Desktop\FYP\AniGEN-Motions\xbot'
+RENDER_PATH = r'C:\Users\User\Desktop\FYP\AniGEN\public'
+MOTIONS = []
+TOTAL_FRAMES = 200
+# Status and code for the notification receiver
+CODE = 'N'
+STATUS = -1
