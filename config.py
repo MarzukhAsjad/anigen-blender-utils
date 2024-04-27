df31104d35b0d06a68bec908be4a819723af98bf
MarzukhAsjad <marz.asjad00@gmail.com>
2024-04-27 20:45:36 +0800

    Update config.py with absolute file paths and increase TOTAL_FRAMES to 300

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Configuration for main.py
-# Unsure if relative paths will work. Try to give absolute filepaths. 
+# Relative paths work if they are in the same directory as the main.py file. Try to use absolute paths for the paths in the config.py file.  
 BLEND_PATH = r'Xbot.blend'
-IMPORT_PATH = r'..\Motions' # Make sure the .fbx motions are there in this directory
-RENDER_PATH = r'..\output' # Make sure this directory exists
-MOTIONS = ['idle', 'walk', 'run'] # Update the array with the list of filenames for example ["Walk.fbx", "Run.fbx", "Drinking_water.fbx"] DO NOT USE SPACES IN FILENAMES
-TOTAL_FRAMES = 200
+IMPORT_PATH = r'C:\Users\User\Desktop\FYP\AniGEN-Motions\ybot' # Give absolute paths here
+RENDER_PATH = r'C:\Users\User\Desktop\FYP\Renders' # Give absolute paths here
+MOTIONS = ['male_strut_walk', 'running_with_intention', 'dodging_to_the_right_place', 'hook_punch', 'covering_face_in_shame_after_defeat', 'idle'] # Give the names of the motions here (do not use .fbx extension here and do not give motions with spaces in their names)
+TOTAL_FRAMES = 300
