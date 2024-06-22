6ec997a33c3aaf6f7e6a34e7c718b3b9d4da8ed9
MarzukhAsjad <marz.asjad00@gmail.com>
2024-06-23 00:57:36 +0800

    Update config file format for flask-app support

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 # Configuration for main.py
-# Relative paths work if they are in the same directory as the main.py file. Try to use absolute paths for the paths in the config.py file.  
-BLEND_PATH = r'Xbot.blend'
-IMPORT_PATH = r'C:\Users\User\Desktop\FYP\AniGEN-Motions\ybot' # Give absolute paths here
-RENDER_PATH = r'C:\Users\User\Desktop\FYP\Renders' # Give absolute paths here
-MOTIONS = ['male_strut_walk', 'running_with_intention', 'dodging_to_the_right_place', 'hook_punch', 'covering_face_in_shame_after_defeat', 'idle'] # Give the names of the motions here (do not use .fbx extension here and do not give motions with spaces in their names)
-TOTAL_FRAMES = 300
+BLEND_PATH = r'path/to/blend/file.blend'
+IMPORT_PATH = r'path/to/directory/containing/motion/files'
+RENDER_PATH = r'path/to/render/output/directory'
+MOTIONS = []
+TOTAL_FRAMES = 200
+# Status and code for external notification receiver
+CODE = 'N'
+STATUS = -1
